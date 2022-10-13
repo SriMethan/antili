c6854591992120be03f5919ac89fa5f580022f9e
SriMethan <methanroyal@gmail.com>
2022-10-13 18:03:54 +0530

    Initial Commit

---
+++ server/custom_trophy_owners.py
@@ -0,0 +1,68 @@
+CUSTOM_TROPHY_OWNERS = {
+    # "username": ("variant", "key in TROPHIES")
+    # if not for any particular variant put "liantichess".
+    "GrohochetGrom": (
+        ("liantichess", "acwc21"),
+    ),
+    "SriMethan": (
+        ("liantichess", "developer"),
+        ("liantichess", "moderator"),
+    ),
+    "cFlour": (
+        ("liantichess", "moderator"),
+    ),
+    "YohaanSethNathan": (
+        ("liantichess", "developer"),
+    ),
+    "Kex09": (
+        ("liantichess", "marathon1"),
+        ("liantichess", "acwc22"),
+    ),
+    "sudenurk2": (
+        ("antichess", "wacwc22"),
+    ),    
+    "ElsaSchneider": (
+        ("liantichess", "marathon5"),
+    ),
+    "ChangeOpening": (
+        ("liantichess", "marathon5"),
+        ("liantichess", "moderator"),
+        ("antichess", "acswc22"),
+    ),
+    "Schizophrenic_Energy": (
+        ("liantichess", "marathon5"),
+    ),
+    "JackDanielsHoney": (
+        ("liantichess", "marathon5"),
+    ),
+    "Kotov_Syndrome": (
+        ("liantichess", "marathon15"),
+    ),
+    "Raicosta": (
+        ("liantichess", "marathon15"),
+    ),
+    "Nikhil_Nikhi": (
+        ("liantichess", "marathon15"),
+    ),
+    "basschess_1989": (
+        ("liantichess", "marathon15"),
+    ),
+    "RexIustus": (
+        ("liantichess", "marathon15"),
+    ),
+    "aleksschtin": (
+        ("liantichess", "marathon15"),
+    ),
+    "kuhar": (
+        ("liantichess", "marathon15"),
+    ),
+    "nicolas647": (
+        ("liantichess", "marathon15"),
+    ),
+    "tolius": (
+        ("liantichess", "marathon15"),
+    ),
+    "Nefilibata": (
+        ("liantichess", "marathon15"),
+    ),
+}
